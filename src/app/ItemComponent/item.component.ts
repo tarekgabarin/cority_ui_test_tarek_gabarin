6fff9ed2ed758b38ed87a84e6222ed8e85bf1ed5
Tarek Gabarin <tarekgabarin@inside-65-82-221.wireless.utoronto.ca>
2018-07-20 13:54:54 -0400

    user can click on item to make blue border disappear

--- src/app/ItemComponent/item.component.ts
+++ src/app/ItemComponent/item.component.ts
@@ -6,4 +6,16 @@ import {Component} from '@angular/core';
   styleUrls: ['./item.component.css']
 })
 
-export class ItemComponent {}
+export class ItemComponent {
+
+  userClickedOnItem: boolean = false;
+
+  updateLeftBlueBorder() {
+
+    return this.userClickedOnItem ? 'none' : 'solid 0.4em #2473bc';
+
+  }
+  clickOnItem() {
+    this.userClickedOnItem = true;
+  }
+}
