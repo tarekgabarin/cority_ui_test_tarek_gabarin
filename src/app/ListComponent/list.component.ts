079ed4eee2449c6e7fb177297a2c878c5f63ce12
Tarek Gabarin <tarekgabarin@Tareks-MacBook-Air.local>
2018-07-20 18:22:53 -0400

    refactor

--- src/app/ListComponent/list.component.ts
+++ src/app/ListComponent/list.component.ts
@@ -9,39 +9,5 @@ import {ItemModel} from '../ItemComponent/item.model';
 })
 
 export class ListComponent {
-  @Input() items: any[] = [
-    {
-      title: 'Tarek learns Angular',
-      subheading: 'He now knows Angular!',
-      subtext: 'He adds another framework to his techstack!',
-      avatar: 'https://i.kym-cdn.com/entries/icons/original/000/013/564/doge.jpg',
-      date: 'Friday July 20th 2018',
-      isNewItem: true
-    },
-    {
-      title: 'Human throws frisbee',
-      subheading: 'Dog catches frisbee!',
-      subtext: 'Dog is a good boy!',
-      avatar: 'https://i.kym-cdn.com/entries/icons/original/000/013/564/doge.jpg',
-      date: 'Friday July 20th 2018',
-      isNewItem: false
-    },
-    {
-      title: 'Human throws frisbee',
-      subheading: 'Dog catches frisbee!',
-      subtext: 'Dog is a good boy!',
-      avatar: 'https://i.kym-cdn.com/entries/icons/original/000/013/564/doge.jpg',
-      date: 'Friday July 20th 2018',
-      isNewItem: false
-    },
-    {
-      title: 'Human throws frisbee',
-      subheading: 'Dog catches frisbee!',
-      subtext: 'Dog is a good boy!',
-      avatar: 'https://i.kym-cdn.com/entries/icons/original/000/013/564/doge.jpg',
-      date: 'Friday July 20th 2018',
-      isNewItem: false
-    }
-  ];
-
+  @Input() items: any [];
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +9,38 @@ import { Component } from '@angular/core';
 
 
 export class AppComponent {
-  title = 'Cority UI Engineer Test';
+  @Input() items: any[] = [
+    {
+      title: 'Tarek learns Angular',
+      subheading: 'He now knows Angular!',
+      subtext: 'He adds another framework to his techstack!',
+      avatar: 'https://i.kym-cdn.com/entries/icons/original/000/013/564/doge.jpg',
+      date: 'Friday July 20th 2018',
+      isNewItem: true
+    },
+    {
+      title: 'Human throws frisbee',
+      subheading: 'Dog catches frisbee!',
+      subtext: 'Dog is a good boy!',
+      avatar: 'https://i.kym-cdn.com/entries/icons/original/000/013/564/doge.jpg',
+      date: 'Friday July 20th 2018',
+      isNewItem: false
+    },
+    {
+      title: 'Human throws frisbee',
+      subheading: 'Dog catches frisbee!',
+      subtext: 'Dog is a good boy!',
+      avatar: 'https://i.kym-cdn.com/entries/icons/original/000/013/564/doge.jpg',
+      date: 'Friday July 20th 2018',
+      isNewItem: false
+    },
+    {
+      title: 'Human throws frisbee',
+      subheading: 'Dog catches frisbee!',
+      subtext: 'Dog is a good boy!',
+      avatar: 'https://i.kym-cdn.com/entries/icons/original/000/013/564/doge.jpg',
+      date: 'Friday July 20th 2018',
+      isNewItem: false
+    }
+  ];
 }
